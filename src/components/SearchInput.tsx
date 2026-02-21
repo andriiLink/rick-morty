5e707059f829359e15fcda0673dc4bc043a73306
Andrii Linkevych <andrii.linkevych.21@pnu.edu.ua>
2026-02-21 13:19:52 +0200

    feat: add all logic to input, changed mata data

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,18 +1,39 @@
 "use client";
 
 import { useDebouncedCallback } from 'use-debounce';
+import { useRouter, usePathname, useSearchParams } from 'next/navigation';
 
 type Params = {
   labelText: string,
 };
 
 export const SearchInput: React.FC<Params> = ({ labelText }) => {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  const handleSearch = useDebouncedCallback((qwery: string) => {
+    const params = new URLSearchParams(searchParams);
+
+    if (qwery) {
+      params.set('name', qwery);
+    } else {
+      params.delete('name');
+    }
+
+    params.set('page', '1');
+
+    replace(`${pathname}?${params.toString()}`);
+  }, 500);
+
   return (
     <div className="flex flex-col items-center gap-5 w-1/2">
       <label className="text-5xl font-semibold">{labelText}</label>
       <div className="flex flex-row justify-between w-full gap-4">
-        <input 
+        <input
           placeholder="Search for characters"
+          onChange={(e) => { handleSearch(e.target.value) }}
+          defaultValue={searchParams.get('name') ?? undefined}
           className="
             placeholder-gray-500
             placeholder:font-light
@@ -27,14 +48,6 @@ export const SearchInput: React.FC<Params> = ({ labelText }) => {
             focus:shadow-blue-500/50
             font-semibold
           "></input>
-        <div 
-          className="bg-blue-500 text-white p-2 rounded font-medium
-            cursor-pointer
-          "
-          onClick={() => {}}
-        >
-          Search
-        </div>
       </div>
     </div>
   );
